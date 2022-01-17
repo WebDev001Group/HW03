18d2eadd8fceafae0f4b027ccd2b027bb79c6696
Alireza Eiji <AlirezaEiji151379@gmail.com>
2022-01-17 16:38:45 +0330

    verify sign up

--- Server/middleware/verifySignUp.js
+++ Server/middleware/verifySignUp.js
@@ -1,3 +1,35 @@
-const db = require("../models")
+const db = require("../models");
+const User = db.users;
 
 
+checkUsernameDuplicate = (req, res, next) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(user => {
+        if (user) {
+            res.status(400).send({ message: "username is already taken!" });
+            return;
+        }
+        next();
+    });
+};
+
+
+
+checkInvalidPassword = (req, res, next) => {
+    if (req.body.password.length < 6) {
+        res.status(400).send({ message: "the password must at least 6 characters" })
+    }
+    next();
+};
+
+
+
+const verifySignUp = {
+    checkUsernameDuplicate,
+    checkInvalidPassword
+};
+
+module.exports = verifySignUp;
